1827f1105236c60aa5cf375693ecce70b55db230
rorycl <gitrl@campbell-lange.net>
2023-11-08 10:26:29 +0000

    fix bad merge

--- walker.go
+++ walker.go
@@ -7,45 +7,6 @@ import (
 	"strings"
 )
 
-// check directory exists
-/*
-func checkDirExists(dir string) bool {
-	d, err := os.Open(dir)
-	if err != nil {
-		return false
-	}
-	defer d.Close()
-
-	info, err := d.Stat()
-	if err != nil {
-		return false
-	}
-	if !info.IsDir() {
-		return false
-	}
-	return true
-}
-*/
-
-// fileChan is a chan of file paths
-var fileChan = make(chan string)
-
-// Exiter indirect os.Exit
-var Exiter = os.Exit
-
-// walker generates a go routine
-func walker(directory string) <-chan string {
-	go func() {
-		defer close(fileChan)
-		err := filepath.Walk(directory, walkerFindEmails)
-		if err != nil {
-			fmt.Println("walk error:", err)
-			Exiter(1)
-		}
-	}()
-	return fileChan
-}
-
 // fileChan is a chan of file paths
 var fileChan = make(chan string)
 
